473abc9668c28f7a61f47635e49e5f3d0ba6ca57
Puneet <puneetgupta18190@gmail.com>
2014-06-12 02:09:50 +0530

    update

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module NokogiriApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += ['bootstrap.css', 'bootstrap.js']
+    config.serve_static_assets = true
+    config.assets.precompile += %w( bootstrap.css )
+    config.assets.precompile += %w( bootstrap.js )
   end
 end
