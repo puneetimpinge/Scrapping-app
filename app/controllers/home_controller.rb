de3834dc2b1a90c659dc796463e1c5ac01b74a85
sanjeev <sanjeev.impinge@gmail.com>
2014-07-24 18:50:14 +0530

    customize app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,33 @@ class HomeController < ApplicationController
   require 'open-uri'
 
   def index
-    if params.has_key?('data')
-      doc = Nokogiri::HTML(open(params[:data][:link]))
 
-      @info = []
-
-      doc.xpath("//#{params[:data][:type]}").each do |link|
-        @info << link.content.to_s
+  end
+  def view_data
+    if params.has_key?('url')
+      doc = Nokogiri::HTML(open(params[:url]))
+      name_selector = JSON.parse(params[:row_data])
+      @table_headers = []
+      @selected_data = []
+      name_selector.each do |obj|
+        selector = obj["selector"]
+        @selected_data << get_data(doc, selector)
+        @table_headers << obj["name"]
       end
-
     end
+  end
+  private
+
+  def get_data(doc,selector)
+    info = []
+    #doc.xpath("//#{params[:data][:type]}").each do |link|
+    #  info << link.content.to_s
+    #end
+    a1 = selector.split(',')[0]
+    a2 = selector.split(',')[1]
+    doc.css("#{a1}").each do |link|
+      info << link.css("#{a2}").text
+    end
+    info
   end
 end
